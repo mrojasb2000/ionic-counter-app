a389be400e09a53017049601026904cfeb12f2cf
Mauricio Rojas <mavro@MacBook-Air-de-Mauricio.local>
2026-04-24 21:23:28 -0400

    refactor: add button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonContent,
+  IonButton,
+  IonFooter
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonFooter,
+    IonContent,
+    IonButton
+  ],
 })
 export class HomePage {
   constructor() {}
